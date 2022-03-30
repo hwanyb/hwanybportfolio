11a46208494334863e59b26b2a55d626c1acdd5a
hwanyb <mnn9502@naver.com>
2022-03-30 11:00:00 +0900

    SearchBar component

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,5 +1,32 @@
 <template>
     <div>
-        Search Bar
+        <v-text-field
+            v-model="title"
+            label="Movie Title" prepend-inner-icon="mdi-magnify"
+            @keypress.enter="searchMovies">
+            <template v-slot:append>
+                <v-progress-circular
+                    size="20"
+                    color="primary"
+                    indeterminate
+                    v-if="loading" />
+            </template>
+        </v-text-field>
     </div>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            title: '',
+            loading: false
+        }
+    },
+    methods: {
+        searchMovies() {
+            console.log('Searched!')
+        }
+    }
+}
+</script>
