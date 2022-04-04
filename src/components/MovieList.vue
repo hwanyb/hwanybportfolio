97d002f31e03af14a67a72595c06ed29e8a876b3
hwanyb <mnn9502@naver.com>
2022-04-04 11:00:00 +0900

    Print movie list

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -1,5 +1,24 @@
 <template>
-    <div>
-        Movie List
-    </div>
+        <v-row>
+            <v-col
+                v-for="movie in movies"
+                :key="movie.imdbID"
+                cols="12"
+                lg="3"
+                md="4"
+                sm="6">
+                <div>{{ movie.Title}}</div>
+                <div>{{ movie.Year}}</div>
+                <div>{{ movie.Poster}}</div>
+            </v-col>
+        </v-row>
 </template>
+<script>
+export default {
+    computed: {
+        movies () {
+         return this.$store.state.movie.movies   
+        }
+    }
+}
+</script>
