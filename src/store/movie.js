8613ddf73905d2ce51ad7323e9a5c74dae3a80d9
hwanyb <mnn9502@naver.com>
2022-04-08 11:00:00 +0900

    Set number of search results

--- src/store/movie.js
+++ src/store/movie.js
@@ -7,13 +7,15 @@ export default {
     loading: false,
     movies: [],
   }),
-  getters: {},
   mutations: {
     updateState(state, payload) {
       Object.keys(payload).forEach((key) => {
         state[key] = payload[key]
       })
     },
+    pushIntoMovies(state, movies) {
+        state.movies.push(...movies)
+    }
   },
   actions: {
     async searchMovies({ state, commit }) {
@@ -21,11 +23,23 @@ export default {
         loading: true,
       })
       const res = await axios.get(
-        `http://www.omdbapi.com/?apikey=cbed7158&s=${state.title}`
+        `http://www.omdbapi.com/?apikey=cbed7158&s=${state.title}&page=1`
       )
+      const pageLength = Math.ceil(res.data.totalResults / 10)
       commit("updateState", {
         movies: res.data.Search,
-        loading: false,
+      })
+
+      if(pageLength > 1) {
+        for(let i = 2; i <= pageLength; i += 1) {
+            if(i > 4) break
+            const resMore = await axios.get(
+                `http://www.omdbapi.com/?apikey=cbed7158&s=${state.title}&page=${i}`)
+            commit('pushIntoMovies', resMore.data.Search)
+        }
+      }
+      commit("updateState", {
+        loading: false
       })
     },
   },
